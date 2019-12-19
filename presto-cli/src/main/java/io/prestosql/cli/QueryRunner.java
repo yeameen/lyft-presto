6b43388d0fadc539cacddad47da8f564093775a0
Chowdhury Yeameen <cyeameen@lyft.com>
2019-12-18 17:02:06 -0800

    Use Lyft Okta for authentication

--- presto-cli/src/main/java/io/prestosql/cli/QueryRunner.java
+++ presto-cli/src/main/java/io/prestosql/cli/QueryRunner.java
@@ -13,14 +13,28 @@
  */
 package io.prestosql.cli;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.net.HostAndPort;
 import io.prestosql.client.ClientSession;
 import io.prestosql.client.SocketChannelSocketFactory;
 import io.prestosql.client.StatementClient;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.Closeable;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
@@ -65,7 +79,8 @@ public class QueryRunner
             Optional<String> kerberosConfigPath,
             Optional<String> kerberosKeytabPath,
             Optional<String> kerberosCredentialCachePath,
-            boolean kerberosUseCanonicalHostname)
+            boolean kerberosUseCanonicalHostname,
+            boolean useOkta)
     {
         this.session = new AtomicReference<>(requireNonNull(session, "session is null"));
         this.debug = debug;
@@ -82,6 +97,7 @@ public class QueryRunner
         setupHttpProxy(builder, httpProxy);
         setupBasicAuth(builder, session, user, password);
         setupTokenAuth(builder, session, accessToken);
+        setupOktaAuth(builder, session, useOkta);
 
         if (kerberosRemoteServiceName.isPresent()) {
             checkArgument(session.getServer().getScheme().equalsIgnoreCase("https"),
@@ -160,9 +176,175 @@ public class QueryRunner
             Optional<String> accessToken)
     {
         if (accessToken.isPresent()) {
-            checkArgument(session.getServer().getScheme().equalsIgnoreCase("https"),
-                    "Authentication using an access token requires HTTPS to be enabled");
+//            checkArgument(session.getServer().getScheme().equalsIgnoreCase("https"),
+//                    "Authentication using an access token requires HTTPS to be enabled");
             clientBuilder.addInterceptor(tokenAuth(accessToken.get()));
         }
+    }
+
+    private static void setupOktaAuth(
+            OkHttpClient.Builder clientBuilder,
+            ClientSession session,
+            boolean useOkta) {
+        if (useOkta) {
+            System.out.println("Asking for okta authentication");
+            User user = new User();
+            Server server = new Server(5000);
+            server.setHandler(new AuthenticationHandler(server, user));
+
+            try {
+                server.start();
+
+                // Open browser
+                Desktop desktop = java.awt.Desktop.getDesktop();
+                URI loginUrl = new URI("http://localhost:5000/login");
+                desktop.browse(loginUrl);
+
+                server.join();
+                System.out.println("Server exited");
+                System.out.println("Access Token: " + user.getAccessToken());
+                // TODO: This should set access token
+                setupTokenAuth(clientBuilder, session, Optional.ofNullable(user.getAccessToken()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class User {
+    String email;
+    String accessToken;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
+
+
+class AuthenticationHandler extends AbstractHandler {
+
+    String REDIRECT_URI = "http://localhost:5000/authorization-code/callback";
+
+    String AUDIENCE = "api://default";
+    String STATE = "SomeState";
+
+    String CLIENT_ID = "0oacleef3oX94aQxj1t7";
+    String CLIENT_SECRET = "WEwnl6HTSZbwBak5UGuXbq1ygc7SxLxGzMONL-4k";
+    String BASE_URL = "https://lyft.okta.com";
+    String ISSUER = BASE_URL + "/oauth2/default";
+    String TOKEN_ENDPOINT = BASE_URL + "/oauth2/default/v1/token";
+    String LOGIN_ENDPOINT = BASE_URL + "/oauth2/default/v1/authorize";
+    String LOGIN_URL = LOGIN_ENDPOINT + "?"
+            + "client_id=" + CLIENT_ID + "&"
+            + "redirect_uri=" + REDIRECT_URI + "&"
+            + "response_type=code&"
+            + "scope=openid&"
+            + "state=" + STATE;
+
+    private Server server;
+    private User user;
+
+    AuthenticationHandler(Server server, User user) {
+        this.server = server;
+        this.user = user;
+    }
+
+    @Override
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException {
+        baseRequest.setHandled(true);
+
+        if (target.equalsIgnoreCase("/hello")) {
+            handleHello(baseRequest, response);
+        } else if (target.equalsIgnoreCase("/authorization-code/callback")) {
+            handleCallback(request, response);
+        } else if (target.equalsIgnoreCase("/login")) {
+            handleLogin(response);
+        }
+    }
+
+    private void handleHello(Request baseRequest, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+
+    private void handleCallback(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Read the code
+        response.getWriter().println("<p>Handling callback</p>");
+        String code = request.getParameter("code");
+        response.getWriter().println("<p>Code: " + code + "</p>");
+
+        // Now get the auth token
+        RequestBody formBody = new FormBody.Builder()
+                .add("grant_type", "authorization_code")
+                .add("code", code)
+                .add("redirect_uri", REDIRECT_URI)
+                .add("client_id", CLIENT_ID)
+                .add("client_secret", CLIENT_SECRET)
+                .build();
+
+        okhttp3.Request accessTokenRequest = new okhttp3.Request.Builder()
+                .url(TOKEN_ENDPOINT)
+                .addHeader("User-Agent", "OkHttp Bot")
+                .post(formBody)
+                .build();
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Response accessTokenResponse = okHttpClient.newCall(accessTokenRequest).execute();
+        String accessTokenResponseBody = accessTokenResponse.body().string();
+        response.getWriter().println("<p>Auth Response: " + accessTokenResponseBody + "</p>");
+
+        // Parse the token
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode parsedJson = mapper.readTree(accessTokenResponseBody);
+        String accessToken = parsedJson.get("access_token").toString();
+        response.getWriter().println("<p>Access Token: " + accessToken + "</p>");
+
+        response.getWriter().println("<a onclick='window.close();'>Close Window</a>");
+
+//        response.getWriter().println("<p>Stopping the server</p>");
+        response.flushBuffer(); // Necessary to show output on the screen
+
+        // Set the user
+        user.setAccessToken(accessToken);
+
+        // Stop the server.
+        try {
+            new Thread(() -> {
+                try {
+                    System.out.println("Shutting down Jetty...");
+                    server.stop();
+                    System.out.println("Jetty has stopped.");
+                } catch (Exception ex) {
+                    System.out.println("Error when stopping Jetty: " + ex.getMessage());
+                }
+            }).start();
+
+        } catch (Exception e) {
+            System.out.println("Cannot stop server");
+            e.printStackTrace();
+        }
+    }
+
+    private void handleLogin(HttpServletResponse response) throws IOException {
+        response.sendRedirect(LOGIN_URL);
     }
 }
